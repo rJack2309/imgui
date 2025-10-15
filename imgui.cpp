6f51d6595d9b08dedc06657a12aecf462e8167fa
rJack2309 <bpostepski@gmail.com>
2025-10-15 15:12:51 +0200

    win!=null

--- imgui.cpp
+++ imgui.cpp
@@ -3836,7 +3836,7 @@ void ImGui::SetActiveID(ImGuiID id, ImGuiWindow* window)
     ImGuiContext& g = *GImGui;
 
 
-    for (int i = 0; i < window->DC.Layouts.Data.Size; i++)
+    for (int i = 0; window!= nullptr && i < window->DC.Layouts.Data.Size; i++)
     {
         ImGuiLayout* layout = (ImGuiLayout*)window->DC.Layouts.Data[i].val_p;
         IM_DELETE(layout);
